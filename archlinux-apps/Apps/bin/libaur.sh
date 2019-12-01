6f983c8329c157f80419b607ecf8518f189c7186
Justin Yang <linuxjustin@gmail.com>
2019-12-01 17:22:43 +0800

    Replace cower with yaah since the former is deprecated for a long time.

--- archlinux-apps/Apps/bin/libaur.sh
+++ archlinux-apps/Apps/bin/libaur.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 #
-# Helper functions for cower
+# Helper functions for yaah
 
 WORKING_DIR=/tmp/buildZone/
 ARCH=`uname -m`
@@ -9,17 +9,14 @@ if [ ! -d "$WORKING_DIR" ]; then
     mkdir -p $WORKING_DIR
 fi
 
-function install_cower {
-    echo === cower is not available in your PATH, so I try to install it ===
-    if [[ "$ARCH" == "armv7l" || "$ARCH" == "aarch64" ]]; then
-        sudo pacman -S cower
-    else
+function install_yaah {
+    echo === yaah is not available in your PATH, so I try to install it ===
         cd $WORKING_DIR
-        git clone https://aur.archlinux.org/cower.git
-        cd cower && makepkg -scCfi
-    fi
+        wget -c https://aur.archlinux.org/cgit/aur.git/snapshot/yaah.tar.gz
+        tar xzf yaah.tar.gz
+        cd yaah && makepkg -scCfi
 }
 
-function test_if_cower_is_available {
-    command -v cower >/dev/null 2>&1 
+function test_if_yaah_is_available {
+    command -v yaah >/dev/null 2>&1 
 }
