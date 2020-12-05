5424c28b928f94505a5a7751d290e9640c93e96f
Justin Yang <linuxjustin@gmail.com>
2020-12-05 22:18:22 +0800

    change fish config dir

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@
 set -x XMODIFIERS "@im=fcitx"
 set -x GTK_IM_MODULE fcitx
 set -x QT_IM_MODULE fcitx
-set -x PATH $PATH /sbin $HOME/Apps/bin
+set -x PATH $PATH /sbin $HOME/Apps/bin $HOME/bin
 set -x EDITOR vim
 set -x PAGER less
 set -x DISPLAY :1
