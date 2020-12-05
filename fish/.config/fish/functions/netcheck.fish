f1278729d97b8c5db43cce3b1af738fbeb8c4ff8
Justin Yang <linuxjustin@gmail.com>
2020-12-05 23:15:22 +0800

    omit output of uname

--- fish/.config/fish/functions/netcheck.fish
+++ fish/.config/fish/functions/netcheck.fish
@@ -1,7 +1,7 @@
 function netcheck
-    if uname | grep -i linux
+    if uname | grep -i linux > /dev/null
         command nload -u K
-    else if uname | grep -i openbsd
+    else if uname | grep -i openbsd > /dev/null
         command systat if 1
     end
 end
