f70ef06b853555e33a7ae684f7cd668fdc5d3dab
Justin Yang <linuxjustin@gmail.com>
2021-01-20 02:00:01 +0800

    Add new path, since some package managers (pip) like it

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@
 set -x XMODIFIERS "@im=fcitx"
 set -x GTK_IM_MODULE fcitx
 set -x QT_IM_MODULE fcitx
-set -x PATH $PATH /sbin $HOME/Apps/bin $HOME/bin $HOME/.cargo/bin
+set -x PATH $PATH /sbin $HOME/Apps/bin $HOME/bin $HOME/.cargo/bin $HOME/.local/bin
 set -x EDITOR nvim
 set -x PAGER less
 
