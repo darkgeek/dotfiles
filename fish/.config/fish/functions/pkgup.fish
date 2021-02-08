198c35574912d185145f7e2e31cc3986e186d7c2
Justin Yang <linuxjustin@gmail.com>
2021-02-08 20:29:52 +0800

    improve on pkgup function

--- fish/.config/fish/functions/pkgup.fish
+++ fish/.config/fish/functions/pkgup.fish
@@ -2,14 +2,14 @@ function pkgup
     if command -qs termux-info
         # if in Termux environment, use pkg
         command pkg up -y $argv
-    else if uname | grep -i openbsd 
+    else if uname | grep -i openbsd > /dev/null
         doas pkg_add -uv
     else if command -qs brew
         command brew update && brew upgrade
     else if command -qs pacman 
         command pkgup
     else if command -qs apt 
-        sudo apt update && sudo apt upgrade
+        sudo apt update -y && sudo apt upgrade -y
     end
 
     if command -qs fwupdmgr
