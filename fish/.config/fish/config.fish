b1ca9008c5c1b6694c550a81940205606e3a2c99
Justin Yang <linuxjustin@gmail.com>
2025-02-11 17:52:57 +0800

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,12 @@
 bass source /etc/profile
 
 # export envrionment variables
-set -x XMODIFIERS "@im=fcitx"
-set -x GTK_IM_MODULE fcitx
-set -x QT_IM_MODULE fcitx
+set -x GTK_IM_MODULE=fcitx
+set -x QT_IM_MODULE=fcitx
+set -x QT5_IM_MODULE=fcitx
+set -x XMODIFIERS=@im=fcitx
 set -x EDITOR vim
-set -x PATH $PATH /sbin /usr/sbin /usr/pkg/sbin $HOME/Apps/bin $HOME/bin $HOME/.cargo/bin $HOME/.local/bin /usr/games /usr/pkg/java/openjdk8/bin /boot/dietpi /boot/dietpi/misc /boot/dietpi/func
+set -x PATH $PATH /sbin /usr/sbin /usr/pkg/sbin $HOME/Apps/bin $HOME/bin $HOME/.cargo/bin $HOME/.local/bin /usr/games /usr/pkg/java/openjdk8/bin /boot/dietpi /boot/dietpi/misc /boot/dietpi/func $HOME/Applications/pandoc/bin
 set -x PAGER less
 
 # Alias
@@ -23,5 +24,5 @@ if isatty && status is-interactive
     load-ssh-agent
 
     # Eyecandy
-    fortune | cowsay
+    fortune | cowsay -f /usr/share/cowsay/cows/default.cow
 end
